63ab9575d1ec40824f2127c4f848bd2b419db72d
Luiz Furmann <furmannluiz@gmail.com>
2025-10-11 10:00:24 -0300

    adicionado splash e icone

---
+++ app/src/main/java/com/furmannsoft/secutirystudy/view/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.furmannsoft.secutirystudy.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.furmannsoft.secutirystudy.R
+import com.furmannsoft.secutirystudy.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this@SplashActivity, MainActivity::class.java).also {
+                startActivity(it)
+            }
+            finish()
+        }, 3000)
+    }
+}
